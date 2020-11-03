eb982374fd0bd9cc7be129d37c787902d154d7a3
ice12031110 <ice12031110@gmail.com>
2020-11-03 18:02:11 +0800

    <feat>add_authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  def after_sign_in_path_for(_user) exchange_index_path
+  end
 end
---
+++ app/controllers/exchange_controller.rb
@@ -0,0 +1,6 @@
+class ExchangeController < ApplicationController
+  def index 
+    
+  end
+
+end
