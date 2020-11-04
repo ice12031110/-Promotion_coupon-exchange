7b75441fc5914bf758253c21e5f1750347098eda
ice12031110 <ice12031110@gmail.com>
2020-11-04 15:55:09 +0800

    <feat>add_exchange_css

--- app/frontend/styles/index.js
+++ app/frontend/styles/index.js
@@ -1,5 +1,7 @@
 import "tailwindcss/base";
 import "tailwindcss/components";
 import "./application";
+import "./exchange";
 import "tailwindcss/utilities";
+import "@fortawesome/fontawesome-free/css/all.css";
 
