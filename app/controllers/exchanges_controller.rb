e28feb815782a77a1fa456fd502ff5cf6ee334ae
ice12031110 <ice12031110@gmail.com>
2020-11-04 10:47:02 +0800

    <feat>add_company_number_check

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -11,9 +11,18 @@ class ExchangesController < ApplicationController
         @exchange = Exchange.new(params_exchanges)
         if @exchange.save
           @coupon = @exchange.create_coupon(serial_number: [*'a'..'z',*'0'..'9',*'A'..'Z'].sample(10).join )
-          redirect_to exchanges_path, notice: "兌換成功"
+          redirect_to exchanges_path, notice: "已成功兌換成功，您的兌換券序號為 " + @coupon.serial_number
         else
-            render :new
+          @company = Exchange.find_by(company_name: @exchange.company_name)
+          if @company.present?
+            @year = @company.created_at.to_s.split[0].split("-")[0]
+            @month = @company.created_at.to_s.split[0].split("-")[1]
+            @day = @company.created_at.to_s.split[0].split("-")[2]
+            redirect_to exchanges_path, notice: "您已於 " + @year +"年" + @month +"月" + @day +"日兌換過"
+          else
+            redirect_to exchanges_path, notice:"您所填的統一編號有誤，請確認後重新輸入!"
+          end
+
         end
     end
 
--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,4 +1,23 @@
 class Exchange < ApplicationRecord
-    validates :company_name, :company_number, :phone_number, presence: true
-    has_one :coupon
+  validates :company_name, :company_number, :phone_number, presence: true
+  validates_uniqueness_of :company_number
+  validates :company_number, presence: true, numericality: { only_integer: true }
+  validates_length_of :company_number, :is => 8
+  has_one :coupon
+  validate :company_number_check
+
+  def company_number_check
+    result = []
+    serial_array = company_number.to_s.split('')
+    num_array = [1, 2, 1, 2, 1, 2, 4, 1]
+    serial_array.zip(num_array) { |a, b| result << a.to_i * b }
+    sum = 0
+    result.each { |elm| 
+    sum += elm.divmod(10).inject { |s, i| s + i }
+    }
+    return true if (sum % 10 == 0) or (sum % 9 == 9 and serial[5] == 7)
+  end
+
+  
+
 end
