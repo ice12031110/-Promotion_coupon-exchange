28481f0d1597c98ea555d9539fa296423feab75f
ice12031110 <ice12031110@gmail.com>
2020-11-05 11:20:27 +0800

    <fix>解決別的使用者兌換過回覆問題

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -14,7 +14,7 @@ class ExchangesController < ApplicationController
           @coupon = @exchange.create_coupon(serial_number: [*'a'..'z',*'0'..'9',*'A'..'Z'].sample(10).join )
           redirect_to exchanges_path, notice: "已成功兌換成功，您的兌換券序號為 " + @coupon.serial_number
         else
-          @company = current_user.exchanges.find_by(company_name: @exchange.company_name)
+          @company = Exchange.find_by(company_name: @exchange.company_name)
           if @company.present?
             @year = @company.created_at.to_s.split[0].split("-")[0]
             @month = @company.created_at.to_s.split[0].split("-")[1]
